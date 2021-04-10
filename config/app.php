5f9eef71dafe29cff936155a9165d7f9277ad76e
jose-mha <josemhha@gmail.com>
2021-04-09 19:15:21 -0500

    - El corazón de Laravel   - Ciclo de vida de una aplicación en Laravel   - ¿Qué son los service containers?   - ¿Cómo funciona un service container?   - Registro y carga de clases con service providers

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
 
+        App\Providers\RatingProvider::class,
     ],
 
     /*
